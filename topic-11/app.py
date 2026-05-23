2204a76096dac1b3dae7aa24ee9363fa3d8d2898
root <abdul123@gmail.com>
2026-05-23 15:46:17 +0000

    Finishing the health check

--- topic-11/app.py
+++ topic-11/app.py
@@ -1,14 +1,27 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import socket, os
+import socket, time
+
+START_TIME = time.time()
 
 class H(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(
-            f"<h2>Request handled by: {socket.gethostname()}</h2>".encode()
-        )
+        if self.path == '/health':
+            uptime = time.time() - START_TIME
+            if uptime < 10:
+                self.send_response(503)
+                self.end_headers()
+                self.wfile.write(b'not ready yet')
+            else:
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(b'healthy')
+        else:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(
+                f"<h2>Hello from {socket.gethostname()}</h2>".encode()
+            )
     def log_message(self, *a): pass
 
 HTTPServer(('', 8000), H).serve_forever()
